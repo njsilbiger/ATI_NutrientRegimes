7b5d5305f28053d9fc665d28ddb3c13820bc97c0
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2023-01-31 10:50:27 -0800

    minor changes

--- Scripts/ATI.R
+++ Scripts/ATI.R
@@ -89,5 +89,51 @@ biplot <- ggplot(loadingdata)+
   theme(line = element_blank())
 
 finalPlot|biplot
-ggsave(here("Output","nutrientpca.png"), width = 8, height = 6)
+ggsave(here("Outputs","nutrientpca.png"), width = 8, height = 6)
 
+### Same with fDOM included
+
+
+nutrientfdom_data<-data_scaled %>%
+  select(Phosphate_scale:Lignin.like_scale) %>%
+  drop_na(Lignin.like_scale)
+
+pca_nuts_fdom<-princomp(nutrientfdom_data)
+
+data_scaled_pca2<-bind_cols(data_scaled%>%
+                              drop_na(Lignin.like_scale), data.frame(pca_nuts_fdom$scores[,1:2]))
+
+# add the color pallete
+data_scaled_pca2 <- biscale::bi_class(data_scaled_pca2, 
+                                     x = Turbinaria_N, 
+                                     y = Turbinaria_V,
+                                     style = "quantile", 
+                                     dim = 3)
+
+p2<-ggplot(data_scaled_pca2)+
+  geom_point(aes(x = Comp.1, y = Comp.2, color = bi_class),
+             show.legend = FALSE)+
+  facet_wrap(~Year) +
+  theme_bw()+
+  bi_scale_color(pal = bivColPal, dim = 3) +
+  ggnewscale::new_scale("color") +
+  theme(line = element_blank())
+
+# make biplot
+loadingdata2<-data.frame(pca_nuts_fdom$loadings[,1:2])
+
+biplot2 <- ggplot(loadingdata2)+
+  coord_equal() + 
+  geom_text(aes(x=Comp.1, y=Comp.2, label=rownames(loadingdata2)), size = 5, vjust=1, color="red")+
+  geom_segment(aes(x=0, y=0, xend=Comp.1, yend=Comp.2), arrow=arrow(length=unit(0.2,"cm")), alpha=0.75, color="red")+
+  theme_bw()+
+  theme(line = element_blank())
+
+# bring it with the legend
+finalPlot2 <- cowplot::ggdraw() +
+  cowplot::draw_plot(p2, 0, 0, 1, 1)+
+  cowplot::draw_plot(legendRas, 0.725, 0.1, 0.275, 0.3) 
+
+fdom<-finalPlot2|biplot2
+
+ggsave(filename = here("Outputs","nutrientfdompca.png"), plot = fdom, width = 12, height = 6)
